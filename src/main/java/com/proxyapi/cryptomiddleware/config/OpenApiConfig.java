8c99b07d532e78887ee4b1b09e132f806b4eb4b0
jdavidc <josedcolman@gmail.com>
2025-08-19 00:40:59 -0300

    Add OpenApi documentation

---
+++ src/main/java/com/proxyapi/cryptomiddleware/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.proxyapi.cryptomiddleware.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI cryptoMiddlewareOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Crypto Middleware API")
+                        .description("Proxy API para precios de criptomonedas usando CoinGecko")
+                        .version("v1.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentación CoinGecko API")
+                        .url("https://www.coingecko.com/en/api"));
+    }
+}
